baf7a80c82408f7a34619c69d249dd9883d65315
pablovzqzbello <pablo.vazquez.bello@gmail.com>
2024-10-28 23:00:51 +0100

    creacion sql_function.py

--- functions/extraction_data.py
+++ functions/extraction_data.py
@@ -1,10 +1,6 @@
 import requests
 import pandas as pd
-from dotenv import load_dotenv
-import os
-from sqlalchemy import create_engine, engine
-from sqlalchemy.exc import SQLAlchemyError
-import mysql.connector
+
 # funcion para balance
 
 def extract_balance(start_year=2011, end_year=2025, time_trunc='day'):
@@ -258,47 +254,3 @@ def extract_co2(start_year=2011, end_year=2025, time_trunc='day'):
 
 # Función INSERTAR DATOS BBDD
 
-def insert_data(df_demanda, df_balance, df_exchanges, df_generation, df_generation_co2):
-    load_dotenv()
-    config = {
-        'host': os.getenv('HOST'),
-        'user': os.getenv('USER'),
-        'password': os.getenv('PASSWORD'),
-        'database': os.getenv('DATABASE')
-    }
-
-    try:
-        connection = mysql.connector.connect(**config)
-
-        if connection.is_connected():
-            print("Conexión exitosa a la base de datos MySQL")
-            db_info = connection.get_server_info()
-            print("Versión del servidor MySQL:", db_info)
-
-            # Aquí puedes realizar las inserciones a la base de datos
-            try:
-                df_demanda.to_sql('demanda_energia', con=engine, if_exists='append', index=False)
-                print("Datos de demanda insertados correctamente.")
-
-                df_balance.to_sql('balance_energia', con=engine, if_exists='append', index=False)
-                print("Datos de balance insertados correctamente.")
-
-                df_exchanges.to_sql('transacciones_energia', con=engine, if_exists='append', index=False)
-                print("Datos de intercambios insertados correctamente.")
-
-                df_generation.to_sql('generacion_energia', con=engine, if_exists='append', index=False)
-                print("Datos de generación insertados correctamente.")
-
-                df_generation_co2.to_sql('generacion_co2', con=engine, if_exists='append', index=False)
-                print("Datos de generación_co2 insertados correctamente.")
-
-            except SQLAlchemyError as e:
-                print("Ocurrió un error durante la inserción de datos:", str(e))
-
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-
-    finally:
-        if connection.is_connected():
-            connection.close()
-            print("Conexión cerrada.")
