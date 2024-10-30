763136bdb911bd3eeb2f1afa5490d0cd668c6a24
pablovzqzbello <pablo.vazquez.bello@gmail.com>
2024-10-30 17:00:55 +0100

    logo y app.py

--- streamlit/pages/home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from auxiliary.db_connection import get_engine
-import pandas as pd
-
-def show_home():
-    st.title("Exploración de datos de la Red Eléctrica Española")
-    st.write("Desde esta página principal podrás acceder a las secciones que más te llamen la atención de nuestra aplicación")
-    
