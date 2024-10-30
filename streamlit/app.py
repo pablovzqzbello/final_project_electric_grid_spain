c4b8fb749773e8751c94bca392aec150231ed788
pablovzqzbello <pablo.vazquez.bello@gmail.com>
2024-10-30 17:03:54 +0100

    logo y app v2

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,43 +1,29 @@
 import streamlit as st
+import pandas as pd
 from config import configure_page
-from pages.home import show_home
 from dotenv import load_dotenv
-from pages.data_overview import show_data_overview
 import os
-
+from auxiliary import *
 configure_page()
 
 def main():
-    ############
-    st.sidebar.title("Navegación")
-    page = st.sidebar.selectbox("Selecciona la vista", ["Inicio", "Presentación de Datos"])
-
-    # Llama a la función correspondiente según la página seleccionada
-    if page == "Inicio":
-        show_home()
-    elif page == "Presentación de Datos":
-        show_data_overview()
-    ##########
-
-    ### ALGUNA IMAGEN?????
+    st.image("Red_Eléctrica_de_España_(logo).svg.png", caption="Logo Red Eléctrica España")
     st.title("Red Eléctrica de España. Análisis de mercado, sostenibilidad y rendimiento")
 
-    st.header("¿Qué es Redeia, S.A.")
-    st.text(
-            "Redeia Corporación, S.A. es un grupo empresarial mutinacional de origen español fundado el 29 de enero de"
+    st.subheader("¿Qué es Redeia, S.A.?")
+    st.markdown("Redeia Corporación, S.A. es un grupo empresarial mutinacional de origen español fundado el 29 de enero de"
             "1985 que actúa en el mercado energético internacional como operador de sistema eléctrico"
             "Cuenta con una financiación público privada, en la que el Gobierno de España cubre el 20% de las invesiones,"
             "mientras que el 80% lo cubre capital privado.")
 
-    st.header("¿De dónde se obtiene la información?")
-    st.text(
-            "La política de total transparencia de la corporación, sellada y garantizada por organismos públicos, se "
+    st.subheader("¿De dónde se obtiene la información?")
+    st.markdown("La política de total transparencia de la corporación, sellada y garantizada por organismos públicos, se "
             "manifiesta a través de la creación de un API donde los consumidores y diversos usuarios pueden consultar"
             "libremente los datos que Redeia genera a tiempo real."
             "Para más consultas: https://www.ree.es/es/datos/apidatos")
 
-    st.header("Objetivos")
-    st.text("Con los daos obtenidos se pretende realizar un análisis integral de la corporación enfocándose a la "
+    st.subheader("Objetivos")
+    st.markdown("Con los datos obtenidos se pretende realizar un análisis integral de la corporación enfocándose a la "
                 "estructura de nogocio desta empresa desde cuatro pilares funamentales:"
                 "- Generación de energía"
                 "- Balance estructural energético"
