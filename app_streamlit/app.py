30976c0dd8fbb827e992d4d58012728334301d7f
pablovzqzbello <pablo.vazquez.bello@gmail.com>
2024-10-31 14:46:26 +0100

    estoy farmando commits

--- app_streamlit/app.py
+++ app_streamlit/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+from functions.sql_function import extract_data
 
 
 
@@ -16,17 +17,17 @@ def main ():
 
     st.title("Red Eléctrica de España. Investigación de mercado, sostenibilidad y rendimiento")
     st.header("¿Qué es Redeia, S.A.?")
-    st.text("Redeia Corporación, S.A. es un grupo empresarial mutinacional de origen español fundado el 29 de enero de"
+    st.markdown("Redeia Corporación, S.A. es un grupo empresarial mutinacional de origen español fundado el 29 de enero de"
             "1985 que actúa en el mercado energético internacional como operador de sistema eléctrico"
             "Cuenta con una financiación público privada, en la que el Gobierno de España cubre el 20% de las invesiones,"
             "mientras que el 80% lo cubre capital privado.")
     st.header("¿De dónde se obtiene la información?")
-    st.text("La política de total transparencia de la corporación, sellada y garantizada por organismos públicos, se "
+    st.markdown("La política de total transparencia de la corporación, sellada y garantizada por organismos públicos, se "
             "manifiesta a través de la creación de un API donde los consumidores y diversos usuarios pueden consultar"
             "libremente los datos que Redeia genera a tiempo real."
             "Para más consultas: https://www.ree.es/es/datos/apidatos")
     st.header("Objetivos")
-    st.text("Con los daos obtenidos se pretende realizar un análisis integral de la corporación enfocándose a la "
+    st.markdown("Con los daos obtenidos se pretende realizar un análisis integral de la corporación enfocándose a la "
             "estructura de negocio desta empresa desde cuatro pilares funamentales:"
             "\n- Generación de energía"
             "\n- Balance estructural energético"
@@ -41,7 +42,10 @@ def main ():
         #### AÑADIR TABLAS DE SQL
         st.subheader("Demanda del mercado")
         #Filtros
+        df_demanda=extract_data("SELECT fecha, valor_demanda_MW FROM demanda_energia")
         #Visualizaciones
+        fig=px.line(df_demanda, x='fecha', y='valor_demanda_MW')
+        st.plotly_chart(fig)
         st.subheader("Balance energético")
         #Filtros
         #Visualizaciones
