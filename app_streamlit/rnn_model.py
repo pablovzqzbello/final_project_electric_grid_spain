4aa8dc82a0842e92d11c2d2a5ad377a21602aeb4
nescanord <nescanord@gmail.com>
2024-11-07 20:40:32 +0100

    RNN main structure

--- app_streamlit/rnn_model.py
+++ app_streamlit/rnn_model.py
@@ -6,5 +6,34 @@ from functions.sql_function import extract_data
 
 def load_data_from_sql():
     query = 'SELECT valor_demanda_MWW FROM demanda_energia'
-    df = extract(query)
-    return df['valor']
+    df = extract_data(query)
+    return df['valor_demanda_MW'].values
+
+def create_sequences(data, sequence_length=30):
+    X = []
+    y = []
+    for i in range(len(data) - sequence_length):
+        X.append(data[i:i + sequence_length])
+        y.append(data[i + sequence_length])
+    return np.array(X), np.array(y)
+
+def build_and_train_rnn(X, y, sequence_length=30, epochs=20):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+    X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+
+    model = Sequential()
+    model.add(SimpleRNN(units=50, activation='relu', input_shape=(sequence_length, 1)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
+
+    history = model.fit(X_train, y_train, epochs=epochs, validation_data=(X_test, y_test))
+    return model, history
+
+if __name__ == "__main__":
+    demanda_values = load_data_from_sql()
+    X, y = create_sequences(demanda_values)
+
+    model, history = build_and_train_rnn(X, y)
+    print("Entrenamiento completo")
